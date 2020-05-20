3e01b9c0298e8aa8f0852787423c55a746343604
1002syp <wocnm1234ccnm@163.com>
2020-05-20 19:40:22 +0800

    1

--- chileme-sev/router/router.js
+++ chileme-sev/router/router.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')() // 引入路由模块
 const User = require('../controller/user_controller') // 引入用户controller
 const Goods = require('../controller/goods_controller') // 引入商品controller
+const Order = require('../controller/order_controller')
 // 写接口
 // test get
 router.get('/',async ctx => {
@@ -22,7 +23,9 @@ router.post('/api/goods/addGoods', Goods.addGoods) //添加商品接口
 router.get('/api/goods/findGoodsList', Goods.findGoodsList) //查询商品列表
 
 // 订单接口
-
+router.get('/api/order/queryOrderList', Order.queryOrderList)
+router.post('/api/order/addOrder', Order.addOrder)
+router.post('/api/order/deleOrder', Order.deleOrder)
 
 
 // 暴露接口模块
