f73f4415072857aa0e8f545a9853b8c8d3b798ea
1002syp <wocnm1234ccnm@163.com>
2020-05-23 08:31:57 +0800

    1

--- chileme-cli/src/views/md.vue
+++ chileme-cli/src/views/md.vue
@@ -1,9 +1,12 @@
 <template>
     <div>
         <h1>测试接口</h1>
-        <div @click="clickHandle">
+        <div @click='clickHandle'>
             {{str}}  <!--插槽-->
         </div>
+        <hr><hr>
+
+        <div v-for='item in arr' :key='item.id' @click='cnmdbi(`item.url,item.method`)'>{{`url:${item.url} method:${item.method}`}}</div>
     </div>
 </template>
 
@@ -12,15 +15,44 @@ export default {
     //存放组件中的数据
     data(){
         return{
-            str:'cnm是个渣男'
-
+            str:'cnm是个渣男',
+            arr:[
+                {id:1,url:'/api/order/queryOrderList',method:'GET'},
+                {id:2,url:'/api/order/addOrder',method:'POST'},
+                {id:3,url:'/api/order/deleOrder',method:'POST'},
+            ]
         }
     },
     //绑定在组件中的方法
     methods:{
         clickHandle(){
+            //alert(this.str)
+            this.Axios({
+                method:'GET',//请求方式
+                url:'/api/order/getCartList'//接口地址
+            }).then(res =>{//请求成功的回调
+                console.log(res)
+                this.fn3()
+            }).catch(err =>{//请求失败的回调
+                console.log(err)
+            })
+        },
+        cnmdbi(url,method){
+            this.Axios({
+                method:'GET',
+                url:'/api/order/getCartList'
+            }).then(res=>{
+            console.log(res)
+            }).catch(err=>{
+            console.log(err)
+            }) 
+
+        },
+        fn3(){
             alert(this.str)
         }
+
+        
 
 
     }
--- chileme-cli/src/views/nb.vue
+++ chileme-cli/src/views/nb.vue
@@ -1,20 +1,55 @@
 <template>
-    <h1>测试接口<div @click="clickHandle">{{arr}}</div></h1>
+    <div>
+        <h1>测试接口</h1>
+         <div @click="clickHandle">
+            {{arr}}
+        </div>
+        <div v-for='item in qwe' :key='item.id' @click='wscr(`item.url,item.method`)'>{{`url:${item.url} method:${item.method}`}}</div>
+    </div>
 </template>
 <script>
 export default {
     data(){
         return{
-            arr:[1,2,3,45,6,]
+            arr:[1,2,3,45,6,],
+            qwe:[
+                {id:1,url:'/api/order/queryOrderList',method:'GET'},
+                {id:2,url:'/api/order/addOrder',method:'POST'},
+            ]
+            
         }
 
     },
     methods:{
         clickHandle(){
             alert(this.arr)
+            this.Axios({
+                method:'POST',
+                url:'/api/order/setdata'
+            }).then(qwe=>{
+            console.log(qwe)
+            this.sb()
+            }).catch(asd=>{
+            console.log(asd)
+            }) 
+        },
+        wscr(url,method){
+        this.Axios({
+                method:'POST',
+                url:'/api/order/setdata'
+            }).then(qwe=>{
+            console.log(qwe)
+            }).catch(asd=>{
+            console.log(asd)
+            }) 
+        },
+        sb(){
+            alert(this.arr)
         }
 
     }
+    
+    
 
     
 
